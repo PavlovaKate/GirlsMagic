f54e93123753878377c87d709c688671a0154426
irinacova <kovalenko.23@inbox.ru>
2024-05-03 15:33:45 +0300

    feat:new4040

--- components/NotFound.jsx
+++ components/NotFound.jsx
@@ -6,7 +6,7 @@ function notFound() {
             <link rel='stylesheet' href='/styles/style.css' />
             <img
                 className='cat'
-                src='https://ih1.redbubble.net/image.3233345973.6578/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg'
+                src='https://media.tenor.com/HFHx4OnbeoMAAAAi/sp-404.gif'
             ></img>
         </h1>
     );
